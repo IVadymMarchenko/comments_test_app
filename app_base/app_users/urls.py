078da94c1abc6eb0d697d42f7d1689194c7b576e
Vadym Marchenko <you@example.com>
2025-04-24 20:40:48 +0300

    first commit

---
+++ app_base/app_users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.views.decorators.cache import cache_page
+from . import views
+
+
+
+app_name = 'user'
+
+
+urlpatterns = [
+     path("registration/",  views.RegistrationView.as_view(), name="registr"), 
+     path("login/",  views.LoginView.as_view(), name="login"),
+]
